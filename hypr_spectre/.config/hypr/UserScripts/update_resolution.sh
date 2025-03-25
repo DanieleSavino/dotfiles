777e73eddb9e15d41ae945e0fa252fc8d8bf450a
DanieleSavino <daniele.savino0@gmail.com>
2025-03-25 23:27:53 +0100

    Hypr Res

---
+++ hypr_spectre/.config/hypr/UserScripts/update_resolution.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Check input argument and set the monitor configuration
+case "$1" in
+    "4k")
+        echo 'Setting resolution to 3840x2160...'
+        echo 'monitor = eDP-1, 3840x2160@60.0, 0x0, 1.666667' > $HOME/.config/hypr/monitors.conf
+        echo 'Done.'
+        ;;
+    "2k")
+        echo 'Setting resolution to 2560x1440...'
+        echo 'monitor = eDP-1, 2560x1440@60.0, 0x0, 1.066667' > $HOME/.config/hypr/monitors.conf
+        echo 'Done.'
+        ;;
+    "hd")
+        echo 'Setting resolution to 1920x1080...'
+        echo 'monitor = eDP-1, 1920x1080@60.0, 0x0, 0.833333' > $HOME/.config/hypr/monitors.conf
+        echo 'Done.'
+        ;;
+    *)
+        echo 'Invalid Resolution'
+        ;;
+esac
